1745cc190455473d1ed396c5f4512edffc6e785e
Peru <aron7usd@gmail.com>
2026-06-28 19:06:12 -0500

    Update supabase-feed.js

--- supabase-feed.js
+++ supabase-feed.js
@@ -2,5 +2,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const supabase = createClient(
   "https://uxwmodnadidlsfwtshcq.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4d21vZG5hZGlkbHNmd3RzaGNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNzY0MjAsImV4cCI6MjA5NTY1MjQyMH0.V3tNuv2iT9J2WbZbs7bzxf9j9RwF2r-Li4KO_1qQWRo"
+  "eyJhbGci..."
 );
+
+export default supabase;  // ← agregar esto
